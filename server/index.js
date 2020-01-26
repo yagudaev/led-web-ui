5604071a1c751a3f54b20d41e7cdfd94c2079c39
Michael Yagudaev <michaelyagudaev@gmail.com>
2020-01-26 13:28:55 -0800

    update how the app is deployed

--- server/index.js
+++ server/index.js
@@ -5,9 +5,19 @@ const port = 3001
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
+const NODE_ENV = process.env('NODE_ENV')
+
 app.get('/api/demos', async (req, res) => {
   const { stdout, stderr } = await exec('cd ../../rpi-rgb-led-matrix/examples-api-use/; sudo ./demo -D1 --led-rows=64 --led-cols=64 --led-slowdown-gpio=1 --led-scan-mode=0 --led-pixel-mapper="Rotate:90" --led-brightness=10 --led-daemon ./strawberry.ppm -m 0')
   res.send(stdout)
 })
+
+if (NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
